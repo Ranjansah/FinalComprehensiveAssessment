d07c55ca9817b53058e7ae84037f43fde56e1807
Ranjan <sahaadi99@gmail.com>
2022-09-19 13:40:23 +0530

    First commit

---
+++ src/main/java/com/test/doctor/FinalComprehensiveAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.test.doctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalComprehensiveAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalComprehensiveAssessmentApplication.class, args);
+	}
+
+}
